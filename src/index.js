93244a02af31e1124834c7a18c952351e8ee4f68
aprilgom <aprilgom@gmail.com>
2021-12-08 21:06:02 +0900

    eraser

--- src/index.js
+++ src/index.js
@@ -29,23 +29,21 @@ function getAngle(point1, point2){
 }
 
 const graphics = new PIXI.Graphics();
-app.stage.addChild(graphics);
+let canvas = new PIXI.Sprite();
 
-const brush = PIXI.Sprite.from('brush.png').texture
-
-
-let size = 0.01;
+let size = 0.05;
 let brush_alpha = 0.5;
-let brush_spread_n = 0.5/size;
 
-const circle = app.stage.addChild(new PIXI.Graphics()
-    .beginFill(0x000000)
-    .drawCircle(0,0,size*55/2)
-    .endFill()
-);
+app.stage.addChild(canvas);
+app.stage.addChild(graphics);
+let drawingmode = "pencil"
+
+//brush
+const pencil = PIXI.Sprite.from('pencil.png').texture
+
+
 
 let render_screen = app.renderer.screen;
-circle.position.set(render_screen.width / 2, render_screen.height / 2);
 
 
 let isDrawing = false;
@@ -56,26 +54,55 @@ app.stage.interactive = true;
 app.stage.hitArea = render_screen;
 
 
+function draw_pencil(origin_x,origin_y){
+    let brush_spread_n = 0.5/size;
+    for(let i = 0;i < brush_spread_n; i++){
+        let brush_spread_x = (Math.random()-0.5)*size*100;
+        let brush_spread_y = (Math.random()-0.5)*size*100;
+        let x = origin_x + brush_spread_x
+        let y = origin_y + brush_spread_y
+        let brush_spread_size = (Math.random() + 3)/4
+        let w = pencil.width*size*brush_spread_size
+        let h = pencil.height*size*brush_spread_size
+        let brush_spread_angle = Math.random()*Math.PI*2
+        graphics.beginTextureFill({
+            texture: pencil,
+            alpha:brush_alpha,
+            matrix: new PIXI.Matrix(size,0,0,size,x-w/2,y-h/2).rotate(brush_spread_angle)
+        });
+        graphics.drawRect(x-w/2,y-h/2,w,h);
+        graphics.endFill();
+    }
+}
+
+function eraser(origin_x,origin_y){
+    canvas.mask = graphics;
+    graphics
+        .beginFill(0x000000,1)
+        .drawRect(0,0,app.screen.width,app.screen.height)
+        .beginHole()
+        .drawCircle(origin_x,origin_y,55*size*4)
+        .endHole()
+        .endFill();
+    let texture = app.renderer.generateTexture(app.stage)
+    canvas.texture.destroy(true)
+    canvas.texture = texture
+    canvas.mask = null;
+    graphics.clear()
+}
+
 app.stage.addEventListener('pointerdown',(e) => {
     isDrawing = true;
     lastPoint = {x: e.clientX,y: e.clientY};
-    let w = brush.width*size;
-    let h = brush.height*size;
-
-    graphics.beginTextureFill({
-        texture: brush,
-        alpha: brush_alpha,
-        matrix: new PIXI.Matrix(size,0,0,size,lastPoint.x-w/2,lastPoint.y-h/2)
-    });
-    
-    //graphics.beginFill(0x000000);
-    graphics.drawRect(lastPoint.x-w/2,lastPoint.y-h/2,w,h);
-    //graphics.drawCircle(0,0,15);
-    graphics.endFill();
+    if(drawingmode == "pencil"){
+        draw_pencil(lastPoint.x,lastPoint.y)
+    }else{
+        eraser(lastPoint.x,lastPoint.y);
+    }
 })
 
+
 app.stage.addEventListener('pointermove',(e) => {
-    circle.position.copyFrom(e.global);
     if (!isDrawing) return;
     let currentPoint = { x: e.clientX, y:e.clientY};
     let dist = getDist(lastPoint, currentPoint);
@@ -83,29 +110,31 @@ app.stage.addEventListener('pointermove',(e) => {
     for(let i = 0; i <= dist; i++){
         let origin_x = lastPoint.x + (Math.sin(angle) * i);
         let origin_y = lastPoint.y + (Math.cos(angle) * i);
-        for(let j = 0;j < brush_spread_n; j++){
-            let brush_spread_x = (Math.random()-0.5)*size*100;
-            let brush_spread_y = (Math.random()-0.5)*size*100;
-            let x = origin_x + brush_spread_x
-            let y = origin_y + brush_spread_y
-            let brush_spread_size = (Math.random() + 3)/4
-            let w = brush.width*size*brush_spread_size
-            let h = brush.height*size*brush_spread_size
-            graphics.beginTextureFill({
-                texture: brush,
-                alpha:brush_alpha,
-                matrix: new PIXI.Matrix(size,0,0,size,x-w/2,y-h/2)
-            });
-        
-            graphics.drawRect(x-w/2,y-h/2,w,h);
-            //graphics.drawCircle(0,0,15);
-            graphics.endFill();
+        if(drawingmode == "pencil"){
+            draw_pencil(origin_x,origin_y)
+        }else{
+            eraser(origin_x,origin_y);
         }
     }
     lastPoint = currentPoint;
-    
 });
 
 app.stage.addEventListener('pointerup',(e) => {
     isDrawing = false;
+    let texture = app.renderer.generateTexture(app.stage)
+    canvas.texture.destroy(true)
+    canvas.texture = texture
+    if(canvas.mask !== null){
+        canvas.mask = null
+    }
+    graphics.clear()
+    //canvas.destory()
+})
+
+app.stage.addEventListener('rightdown',(e)=>{
+    if(drawingmode == "pencil"){
+        drawingmode = "eraser"
+    }else{
+        drawingmode = "pencil"
+    }
 })
